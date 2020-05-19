270ac8f0071d3fe1d42b6061cfc8b87594cb8b58
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2020-05-19 16:41:55 +0200

    notes: Remove the SyncNote class

--- lib/internal/utils.dart
+++ lib/internal/utils.dart
@@ -2,7 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:potato_notes/data/database.dart';
+import 'package:potato_notes/data/model/content_style.dart';
+import 'package:potato_notes/data/model/image_list.dart';
+import 'package:potato_notes/data/model/list_content.dart';
+import 'package:potato_notes/data/model/reminder_list.dart';
 import 'package:potato_notes/widget/pass_challenge.dart';
+import 'package:recase/recase.dart';
 
 class Utils {
   static Future<dynamic> showPassChallengeSheet(BuildContext context) async {
@@ -16,7 +22,44 @@ class Utils {
       ),
     );
   }
+
   static bool statusFromResponse(Response response){
     return json.decode(response.body)["status"];
+  }
+
+  static Map<String, dynamic> toSyncMap(Note note){
+    var originalMap = note.toJson();
+    Map<String, dynamic> newMap = Map();
+    originalMap.forEach((key, value) {
+      var newValue = value;
+      var newKey = ReCase(key).snakeCase;
+      switch(key){
+        case "styleJson":{
+          var style = value as ContentStyle;
+          newValue = json.encode(style.data);
+          break;
+        }
+        case "images":{
+          var images = value as ImageList;
+          newValue = json.encode(images.images);
+          break;
+        }
+        case "listContent":{
+          var listContent = value as ListContent;
+          newValue = json.encode(listContent.content);
+          break;
+        }
+        case "reminders":{
+          var reminders = value as ReminderList;
+          newValue = json.encode(reminders.reminders);
+          break;
+        }
+      }
+      if(key == "id"){
+        newKey = "note_id";
+      }
+      newMap.putIfAbsent(newKey, () => newValue);
+    });
+    return newMap;
   }
 }
